695ada8b750ba20e931ab26ca4fff39c0470443f
Unknown <ab.learn.code@gmail.com>
2018-01-08 16:14:25 +0100

    First commit

---
+++ src/main/java/istic/miage/pallamu/App.java
@@ -0,0 +1,52 @@
+package istic.miage.pallamu;
+
+import java.util.List;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    /**
+     *
+     */
+    private String yoloshirt;
+    /**
+     *
+     */
+    private String yolopants;
+    /**
+     * Ne fait rien
+     * @param yoloman le gars trop cool
+     */
+    public static void yoloswag(String yoloman){}
+
+    /**
+     * play la music
+     * @param playlist la playlist du gars cool
+     */
+    public static void yolomusic(List<String> playlist){}
+
+
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+
+    public String getYoloshirt() {
+        return yoloshirt;
+    }
+
+    public void setYoloshirt(String yoloshirt) {
+        this.yoloshirt = yoloshirt;
+    }
+
+    public String getYolopants() {
+        return yolopants;
+    }
+
+    public void setYolopants(String yolopants) {
+        this.yolopants = yolopants;
+    }
+}
---
+++ src/test/java/istic/miage/pallamu/AppTest.java
@@ -0,0 +1,62 @@
+package istic.miage.pallamu;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+    extends TestCase
+{
+    @org.junit.Test
+    public void yoloswag() throws Exception {
+    }
+
+    @org.junit.Test
+    public void yolomusic() throws Exception {
+    }
+
+    @org.junit.Test
+    public void getYoloshirt() throws Exception {
+    }
+
+    @org.junit.Test
+    public void setYoloshirt() throws Exception {
+    }
+
+    @org.junit.Test
+    public void getYolopants() throws Exception {
+    }
+
+    @org.junit.Test
+    public void setYolopants() throws Exception {
+    }
+
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public AppTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( AppTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
